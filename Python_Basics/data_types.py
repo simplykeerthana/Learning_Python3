7a069c993ae484d7aa438e5b1d10e3607ee41df0
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-23 18:16:04 -0500

    coomi

--- Python_Basics/data_types.py
+++ Python_Basics/data_types.py
@@ -12,11 +12,20 @@
 
 
 #int and float 
+# all numbers are stored in memory as binary. 
 
 print(2+4)
-print( type(2 + 4))
+print( type(2 + 4)) # class 'int'>
 print(2-4)
 print(2*4)
-print(2/4)
+print(type(2/4))
+print(type(0)) # 0 is of a type int
+
+print(type(10.56)) # this stores more in memory because of the floiat type. 
+
+print(5 % 4) # modular to get the remainder is 1
+# float + float is float. 
+
+print( 2 ** 2) # the power of 2 
 
 float 
