1586de347d400e22c73fd1e6047bd6a5fa18d5e5
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-23 16:29:25 -0500

    Update python_intro.py

--- python_intro.py
+++ python_intro.py
@@ -1,1 +1,8 @@
-print("python")
+print('Keerthana Madhavan')
+
+#input will promt the user for input
+input('what is your name')
+
+#using variables assigning name to memory
+name = input('what is your name?')
+print(name)
