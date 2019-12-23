24d082e72e3b44bde25ff468a848383fa045e9b4
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-23 18:51:11 -0500

    added one line variable assignning

--- Python_Basics/variables.py
+++ Python_Basics/variables.py
@@ -3,4 +3,22 @@
 
 iq = 190 
 
-print(iq)
+user_age = iq/4
+
+a = user_age
+
+print(a)
+
+PI = 3.14
+
+#printing everything in a single line
+
+
+a,b,c = 1,2,3
+
+print(a)
+print(b)
+print(c)
+
+#variable naming skill is the important skill for programming
+
