555f3034a308d16b1d2daa7b9306784c2305dc42
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-23 17:07:13 -0500

    commit

---
+++ Python_Basics/data_types.py
@@ -0,0 +1,22 @@
+#data types
+
+# int, float, bool, str, list, tuple, set , dict and none (0)
+
+#A data type is value in python 
+#storing information of that type
+# manipulate data values. they are the fundamental of dattattypes. 
+
+#classes -> are custom data types. 
+
+#specialized data types - packages or modules. 
+
+
+#int and float 
+
+print(2+4)
+print( type(2 + 4))
+print(2-4)
+print(2*4)
+print(2/4)
+
+float 
