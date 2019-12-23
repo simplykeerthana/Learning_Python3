fa3967d02df608882dccfe27841a4027b39503d6
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-23 18:38:45 -0500

    Update data_types.py

--- Python_Basics/data_types.py
+++ Python_Basics/data_types.py
@@ -1,6 +1,6 @@
 #data types
 
-# int, float, bool, str, list, tuple, set , dict and none (0)
+# int, float, bool, str, list, tuple, set , dict and none (0), complex 
 
 #A data type is value in python 
 #storing information of that type
@@ -26,7 +26,7 @@ print(type(10.56)) # this stores more in memory because of the floiat type.
 print(5 % 4) # modular to get the remainder is 1
 # float + float is float. 
 
-print( 2 ** 2) # the power of 2 
+print( 2 ** 2) # the power of 2 , ** power symbol
 
 
 
@@ -35,8 +35,25 @@ print( 2 ** 2) # the power of 2
 print('Rounding numbers using the round() function')
 
 print(round(3.4))
-print(round(3.5))
+print(round(3.5))  
 
 print('absoulte function using the abs() ')
 print(abs(3.4)) #abs of positive num is positive
 print(abs(-3.4))
+
+
+#operator precedence.
+# python interpreter will follow the basic set of rules
+
+print(20-3 * 4)
+
+# precedence of numbers 
+
+# ()
+# **
+# * /
+
+print(bin(1))
+
+print(int('0b101', 2)) # converting the number to base of 2
+
