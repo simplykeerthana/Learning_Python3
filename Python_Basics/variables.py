592857a3682d6b4a53f17f931ba37925e5ede75c
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-23 19:07:29 -0500

    augmented variables
    +=
    -+
    *=
    /=

--- Python_Basics/variables.py
+++ Python_Basics/variables.py
@@ -1,9 +1,9 @@
 #variables
 # ways for us to store information
 
-iq = 190 
+iq = 190 #statement
 
-user_age = iq/4
+user_age = iq/4 #expression
 
 a = user_age
 
@@ -20,5 +20,13 @@ print(a)
 print(b)
 print(c)
 
+
 #variable naming skill is the important skill for programming
 
+#augmented assignment operator
+some_value = 5
+
+some_value += 5
+print(some_value)
+some_value -= 1
+print(some_value)
