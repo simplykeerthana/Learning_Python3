b2f3235de17f7058b936e0a4c4c75ec4818fb781
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-24 09:58:09 -0500

    using python string methods

---
+++ Python_Basics/built_in_functions.py
@@ -0,0 +1,15 @@
+sample = len('helloooo')
+
+print(sample)
+
+#python string methods
+
+
+quote = 'to be or not to be'
+
+
+print(quote.find('be')) # does be exist in the quote string. 
+
+print(quote.replace('be', 'me'))
+
+print("new quote " + quote)
