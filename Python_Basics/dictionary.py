89f31819d4ba8e00c4ba914213fbf9e54d6b06c0
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-24 15:07:42 -0500

    Create dictionary.py

---
+++ Python_Basics/dictionary.py
@@ -0,0 +1,48 @@
+#python dictionary
+# a dictionary has a key, value
+
+dictionary = {
+    'a':1,
+    'b':2
+}
+
+print(dictionary['b']) #grabs the value of the key 
+
+# a dictionary is an unordered key value pair. we just need the key to grab the values
+
+# when shoud you use a list and dictionary
+#ordered -> lisgs
+
+
+# a dictiinary key needs to be immutable
+# a value that is not change
+# also a key in dictionary should be unique
+
+
+#dictionary methods. 
+
+
+user = {
+
+    'basket' : [1,2,3],
+    'greet' : 'hello',
+    'age' : 20
+}
+
+print(user.get('age'))
+
+print(user.get('age', 55))
+#if age doen't exist then use 55
+
+user2 = dict(name='value')
+print(user2)
+
+print('basket' in user)
+print('size' in user)
+
+print(user.items()) #prints everything in the dictionary
+
+# print(user.clear()) clears the dictionary
+# user2 = user.copy()
+
+print(user.pop('age'))
