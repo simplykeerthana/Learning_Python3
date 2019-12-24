4ff92611c582a06333570b2228b45a7c77534567
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-23 19:38:44 -0500

    Update strings.py

--- Python_Basics/strings.py
+++ Python_Basics/strings.py
@@ -2,4 +2,21 @@
 
 print("Keerthana Madhavan")
 
-print(type("Keerthana Madhavan"))
+print(type("Keerthana Madhavan")) # class <'str'>
+
+# strings can either be in double quotes or single quotes
+userrname = 'supercode'
+
+password = 'supersecret' 
+
+#third way to write strings in python ' ' ' multiple statemetns ' ' '
+#multiple line strings
+
+long_string = '''
+WOW
+O O
+___
+
+'''
+
+print(long_string)
