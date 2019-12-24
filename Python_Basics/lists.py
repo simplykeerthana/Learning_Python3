ce6a805e5fa7e43cf6f1fd2a3227c573207160b5
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-24 11:45:52 -0500

    Create lists.py

---
+++ Python_Basics/lists.py
@@ -0,0 +1,16 @@
+#lists in python
+
+
+li = [1,2,3,4,5] # like array
+
+li2 = ['a','b', 'c']
+
+li3 = [1,2, 'a', True] #mixture of data types
+
+#data structure - a way for us to organize information. 
+
+amazon_cart = ['notebooks', 'sunglasses']
+print(amazon_cart[0])
+print(amazon_cart[1])
+
+#List Slicing 
