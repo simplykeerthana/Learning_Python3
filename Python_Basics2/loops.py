8fe2da29b83a0873b29e6d9b03a9aaf8e80d4804
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-26 19:54:53 -0500

    Update loops.py

--- Python_Basics2/loops.py
+++ Python_Basics2/loops.py
@@ -2,5 +2,12 @@
 
 
 #this for loop prints every character in the string "zero to mastery"
+# each character printed in separate lines
+
+
 for item in "zero to mastery":
     print(item)
+
+for item1 in {1,2,3,4,5}:
+    print(item1)
+    
