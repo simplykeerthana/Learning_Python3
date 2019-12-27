c304765190b62427745d97a750d33ed4a2dd671d
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-26 19:30:00 -0500

    Update methods_funcitons.py

--- Python_Basics2/methods_funcitons.py
+++ Python_Basics2/methods_funcitons.py
@@ -85,12 +85,12 @@ print("using not method " + str(not(True)))
 
 #logical operators exercises
 
-is_magician = True
-is_expert = True
+is_magician = False
+is_expert = False
 
 if is_magician and is_expert:
     print("you are a master magician")
-elif is_magician or is_expert
+elif is_magician and not is_expert:
     print("at least you are getting there")
-else not(is_magician)
-    print("you need magic hours")
+elif not is_magician:
+    print("you need magic powers ")
