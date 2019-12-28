8c04152a8786ca4d6382301153f02c50ddfde611
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-28 11:02:58 -0500

    Create OOP.py

---
+++ OOP_Adv_Python/OOP.py
@@ -0,0 +1,18 @@
+#object oriented programming in python
+
+
+#everythinig in Python is an object
+
+print("all the data types in Python")
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('hi'))
+print(type([]))
+print(type(()))
+print(type({}))
+
+#objects have methods and attributes 
+# you create your own objects using the class keyword. 
+
