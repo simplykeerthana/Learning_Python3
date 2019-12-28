040b5aa4a85f290b8ae1c7a162224f585ceee736
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-28 11:09:53 -0500

    Update OOP.py

--- OOP_Adv_Python/OOP.py
+++ OOP_Adv_Python/OOP.py
@@ -16,3 +16,10 @@ print(type({}))
 #objects have methods and attributes 
 # you create your own objects using the class keyword. 
 
+ #  more and more technology requires thousands of lines. so you need classes
+
+ #for example Amazon Delivery Drones can be broken down into different functionality and each can be its own classes
+
+ #OOP is a paradigm is a way to think about our code and structure the code.
+
+ 
