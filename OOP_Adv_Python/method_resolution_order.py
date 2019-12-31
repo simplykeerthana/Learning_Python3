c2918ebb117c5aa0a5cdc2c79fd8bc818adc212d
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-31 15:48:26 -0500

    Update method_resolution_order.py

--- OOP_Adv_Python/method_resolution_order.py
+++ OOP_Adv_Python/method_resolution_order.py
@@ -1,4 +1,6 @@
 # MRO : Method Resolution Order is a order that python follows when there are multiple inheritance
+# the rule to follow, a method in a heirarchy of classes
+# the algorithm for MRO is Depth First Search
 
 
 class A:
@@ -14,6 +16,11 @@ class D(B,C): #inherits from B, C
     pass
 
 
+print(D.num)
+
+#to see how python goes thorugh the heirarchy use MRO()
+
+print(D.mro()) # D -> B -> C -> A -> Object 
 
 #the diagram of the multiple inheritance
 
@@ -24,4 +31,17 @@ class D(B,C): #inherits from B, C
 #   B        C
 #   \       /
 #     \   /
-        D
+#       D
+
+
+#another example
+
+
+class X: pass
+class Y: pass
+class Z: pass
+class A(X,Y): pass
+class B(Y,Z): pass
+class M(B,A,Z): pass
+
+print(M.__mro__)
