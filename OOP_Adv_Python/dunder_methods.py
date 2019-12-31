4f8e46ba999acb9a6c9da2053f98b14351655c85
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-31 13:42:55 -0500

    Update dunder_methods.py

--- OOP_Adv_Python/dunder_methods.py
+++ OOP_Adv_Python/dunder_methods.py
@@ -7,8 +7,12 @@ class Toy():
     def __str__(self): #you can sometimes modify dunder methods
         return f'{self.color}'
 
+    def __len__(self):
+        return 5
+
 action_figure = Toy('red', 0)
 print(action_figure.__str__()) #dunder methods like __str__() are recognized by python
 
 #the above dunder method can be written like 
 print(str(action_figure))
+print(len(action_figure))
