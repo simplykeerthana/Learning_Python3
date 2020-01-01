b80cff4d16e0fdf596c900a15301d5af019e060d
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-31 19:38:31 -0500

    Update list_comprehensions.py

--- Functional_Programming_OOP/list_comprehensions.py
+++ Functional_Programming_OOP/list_comprehensions.py
@@ -34,3 +34,23 @@ print()
 print()
 print(my_dict)
 print(my_dict1)
+
+#exercise comprehension
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+print(duplicates)
+
+#using comprehension
+
+duplicates1 = []
+
+duplicates1 = list(set([x for x in some_list if some_list.count(x) > 1]))
+
+print(duplicates1)
