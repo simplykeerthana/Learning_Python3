c078d95c845804614515d26625f541786bdb1b3c
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-31 19:26:14 -0500

    Create list_comprehensions.py

---
+++ Functional_Programming_OOP/list_comprehensions.py
@@ -0,0 +1,33 @@
+# list,  set, dictionary.... can be used with comprehensions
+my_list = [char for char in 'hello']
+my_list2 = [num for num in range(0,100)]
+my_list3 = [num**2 for num in range(0,100)]
+
+my_list4 = [num**2 for num in range(0,100) if num % 2 ==0 ]
+
+
+print(my_list4)
+#sets - you get pretty much the same answers as list
+
+my_list = {char for char in 'hello'}
+my_list2 = {num for num in range(0,100)}
+my_list3 = {num**2 for num in range(0,100)}
+
+my_list4 = {num**2 for num in range(0,100) if num % 2 ==0 }
+
+print(my_list)
+print(my_list2)
+print(my_list3)
+print(my_list4)
+
+#dictionary
+
+simple_dict = {
+    'a':1,
+    'b': 2
+}
+my_dict = {key:value**2 for key, value in simple_dict.items()}
+
+print()
+print()
+print(my_dict)
