2624b779e117f18e2ea88437ff444912108956bc
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-31 23:27:26 -0500

    Update errors.py

--- Errors/errors.py
+++ Errors/errors.py
@@ -1,2 +1,11 @@
 # errors and error handling in Python
+#an error that crashes programs are exceptions. 
 
+
+print(1+True)
+
+try:
+    age = int(input("what is your age"))
+    print(age)
+except:
+    print('please enter a number')
