db9c6709406640373159cb32baa2738042a64396
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-31 21:49:09 -0500

    Update decorators.py

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -61,12 +61,16 @@ from time import time
 
 def performance(fn):
     def wrapper(*args, **kwargs):
+        t1 = time()
         result = fn(*args, **kwargs)
+        t2  = time()
+        print(f'took {t2-t1} ms')
         return result
     return wrapper
-@performance 
 
+@performance 
 def long_tiime():
-    for i in range(10000000)
+    for i in range(10000000):
         i*5
 
+long_tiime()
