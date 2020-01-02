5d9ce3c5051814052345c4c36f3ec62d124a1b9c
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-02 16:47:36 -0500

    Create python_debugging.py

---
+++ python_debugging.py
@@ -0,0 +1,16 @@
+#debugging in Python 
+# the act of finding and removing bugs or errors from our code is called debugging
+
+#linting
+#use ide/editor
+# read errors
+
+#python debugger pdb module
+
+import pdb
+
+def add(num1, num2):
+    pdb.set_trace() #it traces the input and output
+    return num1 + num2
+
+add(4, 'hhdfadsklfas')
