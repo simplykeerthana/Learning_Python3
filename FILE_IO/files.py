03136a0d524aee24428addd3e8f7d130076fb275
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-02 17:54:02 -0500

    file, append, write and read

--- FILE_IO/files.py
+++ FILE_IO/files.py
@@ -8,4 +8,42 @@
 
 my_file = open('test.txt')
 
-print(my_file)
+print(my_file.read())  #you can only read the files once, becayse the cursor goes to eof
+
+my_file.seek(0) #to point to the beginning of the file
+
+print(my_file.read())
+
+my_file.seek(0)
+print(my_file.readlines)
+
+
+#you haave to close the files after you open it
+
+my_file.close()
+
+#read, write, append
+
+#r+ read write
+# 'a' hey it appends to the end of the file
+
+with open('test.txt', mode='a') as my_file1:
+    text = my_file1.write(' hey it is me')
+    print(text)
+    #print(my_file1.readlines())
+
+my_file1.close()
+
+#if we don't specify the mode, then it is automatically read
+
+# with open('test.txt', mode='r') as my_file
+
+#write to a file
+
+#creating a file that does not exist
+
+with open('sad.txt', mode='w') as my_file2:
+    text = my_file2.write(':(')
+
+
+#file paths
