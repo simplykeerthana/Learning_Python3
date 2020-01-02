fae27a0cce1fc3dc1e8f94690dd01c06f38cb24c
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-02 14:12:59 -0500

    Update main.py

--- Modules/module1/main.py
+++ Modules/module1/main.py
@@ -6,3 +6,6 @@
 import utility
 
 print(utility) #when you run this statement, python generates a py cache folder 
+
+#to save time, python is going to run the cache file next next time utility is called if there is no change made to the program
+
