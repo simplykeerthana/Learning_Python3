f19614894e72c81c99480b3be41c6ad39a10e3b6
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-02 11:59:06 -0500

    Update generators.py

--- Generators/generators.py
+++ Generators/generators.py
@@ -6,6 +6,7 @@ list(range(100)) # creates and stores 100 elements in memory
 
 
 #creating a list instead of using a list function
+# this list is created in memory
 def make_list(num):
     result = []
     for i in range(num):
@@ -18,9 +19,15 @@ print(my_list)
  #use a generator to access large number of elements in a list, one at a time
 #everything that is a generator is iterable, generator is a subset of a iterable
 
+# whereas this list created already and we use it one by one. 
 def generator_function(num):
-    for i in range(10000000):
-        yield i #give the element 1 by 1
+    for i in range(num):
+        yield i #give the element 1 by 1 
     
-for item in generator_function(1000):
-    print(item)
+#for item in generator_function(1000):
+ #   print(item) 
+
+g = generator_function(100)
+
+print(g())
+
