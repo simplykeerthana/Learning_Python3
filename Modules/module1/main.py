d16553520fdb1fca84db908a4afdbe4a543acf35
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-02 14:29:57 -0500

    Update main.py

--- Modules/module1/main.py
+++ Modules/module1/main.py
@@ -9,7 +9,8 @@
 import utility #you can import many files
 #import shopping_cart, this won't work because shopping_cart is not a module
 
-import Shopping.more_shopping.shopping_cart
+#import Shopping.more_shopping.shopping_cart
+from Shopping.more_shopping.shopping_cart import buy #just import the buy fucntion
 
 print(utility) #when you run this statement, python generates a py cache folder 
 
@@ -17,9 +18,12 @@ print(utility.diivide(2,3))
 print(utility.multiply(2,3))
 #to save time, python is going to run the cache file next next time utility is called if there is no change made to the program
 
-print(Shopping.more_shopping.shopping_cart.buy('apple')) # you have access to shopping_cart module
+#print(Shopping.more_shopping.shopping_cart.buy('apple')) # you have access to shopping_cart module
 
-print(Shopping.more_shopping.shopping_cart.buy('banana'))
+#print(Shopping.more_shopping.shopping_cart.buy('banana'))
+
+print(buy('apple'))
+print(buy('banana'))
 
 
 # a python package creattes an __int__.py automatically, it may or may not be hidden by the IDE
