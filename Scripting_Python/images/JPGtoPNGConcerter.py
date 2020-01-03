5d58ec0d65216166e84513481bdc06e440b95409
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-03 17:01:59 -0500

    update

--- Scripting_Python/images/JPGtoPNGConcerter.py
+++ Scripting_Python/images/JPGtoPNGConcerter.py
@@ -3,17 +3,20 @@ import os #to grabs the path
 from PIL import Image
 
 
+#give the input as Python3 JPGtoPNGConverter Pokedex/ new/
+
+
 # grab the first argument the folder to look in for jpg image
 #use the second arguemnt to create the new folder and save the converted images
 
 path = sys.argv[1]
 directory = sys.argv[2] #the folder to be created
 
-if not os.path.exits(directory)
+if not os.path.exists(directory):
     os.makedirs(directory) #if does not exist then create the folder
     
     
-for filename in os.listdir(path) #loop thorugh the num items in the pokedex folder
+for filename in os.listdir(path): #loop thorugh the num items in the pokedex folder
     clean_name = os.path.splitext(filename)[0]
     img = Image.open(f'{path}{filename}')
     img.save(f'{directory}/{clean_name}.png', 'png')
