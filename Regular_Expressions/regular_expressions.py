ed0604b1bf0ff44d3e5054f75c3589d12a9f48c3
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-02 19:35:08 -0500

    Update regular_expressions.py

--- Regular_Expressions/regular_expressions.py
+++ Regular_Expressions/regular_expressions.py
@@ -6,4 +6,18 @@ string = 'search inside of this text please'
 
 print('search' in string)
 
-print(re.search('this', string))
+a = re.search('this', string)
+
+print(a.span())
+print(a.start())
+print(a.group())
+print(a.end()) 
+
+pattern = re.compile('search this inside the fucntion')
+
+a1 = pattern.search(string)
+b = pattern.findall(string)
+c = pattern.fullmatch(string)
+d = pattern.match(string)
+
+print(c)
