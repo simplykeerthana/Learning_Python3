154c9cc949f1cd8c76c9e1477ffec1add7e12678
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-03 16:36:30 -0500

    Create scripting.py

---
+++ Scripting_Python/scripting.py
@@ -0,0 +1,11 @@
+# scripting in python
+#writing scripts using scripting, the power of python
+
+#process images, convert pdf's and etc.
+
+#what is image processing
+# for ex, instagram process images by cropping pictures, reducing the size and etc
+
+#internet is the best friend, serach for libraries and packages to use instead of reinventing the wheel
+# like for example the translator one we did. 
+# install pillow library for image processing
