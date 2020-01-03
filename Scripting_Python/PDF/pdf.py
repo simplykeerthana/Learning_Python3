6ee020de3fad011ef61420391a21e1a07aaa1ac1
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-03 17:51:30 -0500

    Update pdf.py

--- Scripting_Python/PDF/pdf.py
+++ Scripting_Python/PDF/pdf.py
@@ -31,3 +31,18 @@ def pdf_combiner(pdf_list):
     merger.write('super.pdf')
 
 pdf_combiner(inputs)
+
+
+# a tool that water marks all the pdf's for thousands of files or more
+
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
+                                                    
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+    
+    with open('watermarked_output.pdf', 'wb') as  file1:
+        output.write(file1)
