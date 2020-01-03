59abfd756f739ee6603d69379a1379649a3821c7
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-03 18:43:57 -0500

    Update email_sender.py

---
+++ email_sender.py
@@ -0,0 +1,28 @@
+# sending emaiks with python
+# there are built in libraries to send emails in python. 
+
+#for example mailchimp is a subscription based software 
+
+import smtplib #simple mail transfer protocol library, to create a server
+
+#gmail, hotmail use the smptlib
+
+from email.message import EmailMessage
+
+email = EmailMessage()
+
+email['from'] = 'Keerthana Madhavan'
+email['to'] = 'mkeerthanarockz@gmail.com'
+email['subject'] = 'Prepare yourself to become a software engineer this 2020'
+
+ email.set_content('I will be a python Master!')
+ 
+ #use the smtp server to login to our gmail and send it 
+ #smpt is customed to whatever email you use
+ 
+ with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+     smtp.ehlo()
+     smtp.starttls() #encryption methods
+     
+     
+     
