aaac53c0a8c379079b0e35b8cb41e614cf1bb83c
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-03 17:32:43 -0500

    dummy pdf rotated counte clockwise by 90 degree in a new file tilt

--- Scripting_Python/PDF/pdf.py
+++ Scripting_Python/PDF/pdf.py
@@ -1,1 +1,15 @@
 # pdf's in python
+ #find the best library to process pdf - the best one is PyPDF2
+ 
+import PyPDF2
+ 
+with open ('dummy.pdf', 'rb') as file: #you have to read a binary file
+    reader = PyPDF2.PdfFileReader(file)
+    #print(reader.numPages)
+    page = reader.getPage(0)
+    page.rotateCounterClockwise(90)
+    writer = PyPDF2.PdfFileWriter()
+    writer.addPage(page)
+ #rotate the dummy.pdf by 180%
+    with open('tilt.pdf', 'wb') as new_file:
+         writer.write(new_file)
