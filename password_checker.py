f25eb2a4d280f8c6aa0e7174359e1db39fc172ef
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-03 21:15:31 -0500

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,1 +1,12 @@
 #password checker project
+
+# secure way to check if your password has ever been hacked
+
+# have you been pwned api is good for password checker
+
+# build your own password checker and secure API
+
+import requests
+
+url = 'https://api.pwnedpasswords.com/range/' + '_password123'
+res = requests.get(url)
