a5d650fc5cc72d8d8a9b04f852dfaaef7fcbd843
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-04 11:27:48 -0500

    Update web_scrapping.py

---
+++ Web_Scrapping/web_scrapping.py
@@ -0,0 +1,7 @@
+#web scrapping in python
+#grabbing the data from the website
+# downloading the webpage, gabbing the data from the webpage, cleanign it up and sso it is useful data
+
+
+#use cases of web scrapping
+# http://www.entropywebscraping.com/2017/01/01/big-list-web-scraping-uses/
