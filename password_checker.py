4c0cd4e59a1544569dabde4adcc3f4741acbba8b
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-03 21:55:25 -0500

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -9,9 +9,20 @@
 import requests
 
 #this API uses hasing, make the passwoed gibberish. use an hasing generator - https://passwordsgenerator.net/sha1-hash-generator/
+# you just need 5 characters of the hasg
 
-url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
-res = requests.get(url)
+
 
 print(res) #response 400 is not good, usually means unauthorized, 200 means ok
 
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+
+def pwned_api_check(password):
+    #check password if it exists in API response
+    pass
+
+request_api_data(123)
