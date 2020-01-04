8a638650f5d858cb0244fa11dfbea2d3fc03a54f
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-03 20:38:10 -0500

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -9,20 +9,25 @@ import smtplib #simple mail transfer protocol library, to create a server
 
 from email.message import EmailMessage
 
+# the entire 'email' is an object
+
 email = EmailMessage()
 
 email['from'] = 'Keerthana Madhavan'
 email['to'] = 'mkeerthanarockz@gmail.com'
 email['subject'] = 'Prepare yourself to become a software engineer this 2020'
 
- email.set_content('I will be a python Master!')
+email.set_content('I will be a python Master!')
  
  #use the smtp server to login to our gmail and send it 
  #smpt is customed to whatever email you use
  
- with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
      smtp.ehlo()
      smtp.starttls() #encryption methods
      smtp.login('somthing@gmail.com', 'password')
+     smtp.send_message(email)
+     print('all good, email has been sent')
      
-     
+    
+#this exercise would give you an errar because I gave fake credentials
