7980af6be693aac2c001662e51cdcf85c909d006
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-04 22:16:28 -0500

    update

--- Web_Development_Python/web_server/server.py
+++ Web_Development_Python/web_server/server.py
@@ -1,13 +1,27 @@
+import os
 
-from flask import Flask, render_template
-app = Flask(__name__) 
+
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__)
+ 
 
 #render_template allows you to send html files.
 #print(__name__) #this prints __main__
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Keerthana!, you aree on a road to become a software engineer! ultimate vision for 2020'
+    print( url_for('static', filename='bolt.ico'))
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+#@app.route('/favicon.ico') #to show icon
+#def favicon():
+  #  return send_from_directory(os.path.join(app.root_path, 'static'),
+                            #'favicon.ico', mimetype='image/System-software-update.svg.png')
+
 
 @app.route('/blog')
 def blog():
@@ -16,3 +30,5 @@ def blog():
 @app.route('/blog/2020/dogs')
 def blog2():
     return 'this is my dogs'
+
+#for css and java script  you need a static folder
