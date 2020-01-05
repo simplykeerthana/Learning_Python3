e2d93523bed54dbd9d13bee0aa9935b1c94dfd6b
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-05 13:44:24 -0500

    Create ML_Intro.py

---
+++ ML_Data_Science/ML_Intro.py
@@ -0,0 +1,12 @@
+# Machine Learnig Intro and Data Science History
+
+#machines to perforrm tasks really really fast
+# like a game of chess against the computer
+
+#Ml -> self drivng cars, chatbot and etc. to make the machines smarter
+
+#machine learning is a subset of AL. 
+#data science is a overlapping of ml, dl, ai
+
+#data 
+# spreadshseets to relation db - big data (no sql, mongoDB) -> machine learning
