5d0b6cf082013a371a27004c8708a84330bb0533
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-04 20:36:33 -0500

    Update server.py

--- Web_Development_Python/web_server/server.py
+++ Web_Development_Python/web_server/server.py
@@ -1,9 +1,10 @@
 
 from flask import Flask
 app = Flask(__name__) 
-print(__name__) #this prints __main__
+
+#print(__name__) #this prints __main__
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Keerthana!'
 
